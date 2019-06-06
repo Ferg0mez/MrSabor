f3f322c4fa49211df5165556b39fc2f29f113126
Ferg0mez <38075233+Ferg0mez@users.noreply.github.com>
2019-06-06 19:16:20 -0400

    Add files via upload

---
+++ conexion.php
@@ -0,0 +1,14 @@
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:mrsabor.database.windows.net,1433; Database = mrsabor", "adminmrsabor", "{your_password_here}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "adminmrsabor@mrsabor", "pwd" => "{your_password_here}", "Database" => "mrsabor", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:mrsabor.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
